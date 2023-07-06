d2db073af996724abff8e95998eb327380078b73
hansapradhan <pradhan.hansa@gmail.com>
2023-07-06 22:28:57 +0530

    Commit for meme generator

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Component/Navbar';
+import Meme from './Component/Meme';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Meme />
+ </div>
+  );
+}
+
+export default App;
